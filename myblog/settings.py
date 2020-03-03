75cd84bacf3d52ab3aedd206b32d9c741411fbb7
Arglinux <Arglinux@gmail.com>
2020-03-03 01:16:34 -0300

    probando

--- myblog/settings.py
+++ myblog/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images).
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_URL = '/static/'
-STATIC_URL = "/home/Arglinux/arglinux.pythonanywhere.com/static/"
-STATIC_ROOT = "/home/Arglinux/arglinux.pythonanywhere.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
 
 STATICFILES_FINDERS = [
      "django.contrib.staticfiles.finders.FileSystemFinder",
