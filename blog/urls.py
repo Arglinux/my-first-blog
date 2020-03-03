80a2a4e7e780fb3850ecdaafbca90087659b827f
Arglinux <Arglinux@gmail.com>
2020-03-03 14:58:34 -0300

    probando

--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
 	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 	url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
 	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
-] + blog/static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
