b916cd563670aac8d3ff3e738a4ef5ba174b4d7b
Arglinux <Arglinux@gmail.com>
2020-03-03 15:29:03 -0300

    probando

--- myblog/settings.py
+++ myblog/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#SSTATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATIC_ROOT = (os.path.join(SITE_ROOT, 'static/'))
+
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
 #STATICFILES_DIRS = (
 #  os.path.join(SITE_ROOT, 'static/'),
 #)
