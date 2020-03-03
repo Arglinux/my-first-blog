3a0a1720e07292abff95cf8413321095c9c46f88
Arglinux <Arglinux@gmail.com>
2020-03-03 15:08:17 -0300

    probando

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -16,9 +16,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
-if settings.DEBUG:
-    application = StaticFilesHandler(get_wsgi_application())
-else:
-    application = get_wsgi_application()
+#if settings.DEBUG:
+#    application = StaticFilesHandler(get_wsgi_application())
+#else:
+#    application = get_wsgi_application()
