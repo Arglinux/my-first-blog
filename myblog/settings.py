449fbea6716e360633243f3960bd9a8a6f98c3fe
Arglinux <Arglinux@gmail.com>
2020-03-03 00:28:21 -0300

    Añadido de imágenes (static files) al blog.

--- myblog/settings.py
+++ myblog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 ##STATIC_URL = '/static/'
 STATIC_URL = "/home/Arglinux/arglinux.pythonanywhere.com/static/"
-STATIC_ROOT = "/home/Arglinux/arglinux.pythonanywhere.com/static/"
+##STATIC_ROOT = "/home/Arglinux/arglinux.pythonanywhere.com/static/"
 ##STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
